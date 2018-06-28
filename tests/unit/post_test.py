b0348eab321ae293bf82823ca891d75800b3fa59
Nalpacio <mengdong.sun@gmail.com>
2018-06-28 15:39:18 +0200

    first two unittests

---
+++ tests/unit/post_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from post import Post
+
+
+class PostTest(TestCase):
+    def test_create_post(self):
+        post = Post('Title', 'Content')
+
+        self.assertEqual(post.title, 'Title')
+        self.assertEqual(post.content, 'Content')
+
+    def test_json(self):
+        post = Post('Title', 'Content')
+        expected = {'Title': 'Title',
+                    'Content': 'Content',
+                    }
+
+        self.assertDictEqual(post.json(), expected)
